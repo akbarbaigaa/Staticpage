25b4077a01de2154534eb727c7caadd7b0c33890
Akbar Baig <48825804+akbarbaigaa@users.noreply.github.com>
2024-02-24 02:48:39 +0530

    Add files via upload

---
+++ Agency.js
@@ -0,0 +1,54 @@
+$(function(){
+
+    var url = window.location.href; 
+
+    $("#menu a").each(function() {
+
+        if(url == (this.href)) { 
+            $(this).closest("li").addClass("active");
+        }
+    });
+
+});
+
+let links = document.querySelectorAll(".links a");
+let bodyId = document.querySelector("body").id;
+ 
+for(let link of links){
+    if(link.dataset.active == bodyId){
+        link.classList.add("active");
+    }
+}
+
+
+
+// Script for the banner slider 
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active2", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active2";
+}
